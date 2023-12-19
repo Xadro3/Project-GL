a55423d2f81f02c9da405fe49a19e784600b794d
sagtdasnicht <mario.viktor.schneider@gmail.com>
2023-12-19 12:18:23 +0100

    AudioManagerScript + AudioMixer Setup

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------- Audio Source ---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("--------- Breach Audio Clip ---------")]
+    public AudioClip breachStartingInterlude;
+    public AudioClip breachBackgroundLoop;
+    public AudioClip endTurn;
+    public AudioClip pauseActivate;
+    public AudioClip pauseDeactivate;
+    public AudioClip breachLost;
+    public AudioClip breachWon;
+    public AudioClip sceneStart;
+
+    private void Start()
+    {
+        musicSource.clip = breachStartingInterlude;
+        musicSource.Play();
+        
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
