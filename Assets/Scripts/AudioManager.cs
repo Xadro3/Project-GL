43f496c8b065c1ee995f1ea4fe77aee6074534d5
sagtdasnicht <mario.viktor.schneider@gmail.com>
2023-12-20 11:26:05 +0100

    AudioManagement, ButtonParten

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,29 +3,122 @@ using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("--------- Audio Source ---------")]
+    public double musicDuration;
+    public double goalTime;
+    bool isPlaying = false;
+
+    [Header("------------ Audio Source ------------")]
+    //[SerializeField] AudioSource startingLoopSource;
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource sfxSource;
 
-    [Header("--------- Breach Audio Clip ---------")]
+    [Header("------- Pause und Menü Sound -------")]
+    public AudioClip pauseActivate;
+    public AudioClip pauseDeactivate;
+
+    [Header("---------- Breach Audio Clip ----------")]
     public AudioClip breachStartingInterlude;
     public AudioClip breachBackgroundLoop;
     public AudioClip endTurn;
-    public AudioClip pauseActivate;
-    public AudioClip pauseDeactivate;
     public AudioClip breachLost;
     public AudioClip breachWon;
-    public AudioClip sceneStart;
+    public AudioClip breachStart;
+    public AudioClip slotSnap;
+
+    [Header("------------- Hauptmenü -------------")]
+    public AudioClip backgroundmusicMenu;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
 
     private void Start()
     {
-        if (SceneManager.GetActiveScene().name == "Encounter")
+        if (SceneManager.GetActiveScene().name == "Encounter" | SceneManager.GetActiveScene().name == "ParticleTestScene")
         {
             musicSource.clip = breachStartingInterlude;
-            musicSource.Play();      
+            musicSource.Play();
+            musicDuration = (double)breachStartingInterlude.samples / breachStartingInterlude.frequency;
+            //goalTime = goalTime + musicDuration;
+
+            /*
+            goalTime = AudioSettings.dspTime + 0.5;
+            musicSource.clip = breachStartingInterlude;
+            musicSource.PlayScheduled(goalTime);
+            musicDuration = (double)breachStartingInterlude.samples / breachStartingInterlude.frequency;
+            */
+
+            sfxSource.clip = breachStart;
+            sfxSource.Play();   
+        }
+
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            musicSource.clip = backgroundmusicMenu;
+            musicSource.Play();
         }
 
     }
+
+    private void Update()
+    {
+
+
+        if (isPlaying == false && SceneManager.GetActiveScene().name == "Encounter")
+        {
+
+            musicSource.clip = breachStartingInterlude;
+            musicSource.Play();
+            isPlaying = true;
+            musicDuration = (double)breachStartingInterlude.samples / breachStartingInterlude.frequency;
+
+            sfxSource.clip = breachStart;
+            sfxSource.Play();
+        }
+
+        if (isPlaying == true && musicDuration == 0)
+        {
+            musicSource.clip = breachBackgroundLoop;
+            musicSource.Play();
+        }
+
+        if (isPlaying == true && SceneManager.GetActiveScene().name == "Overworld")
+        {
+            musicSource.clip = backgroundmusicMenu;
+            musicSource.Play();
+            isPlaying = false;
+        }
+        
+        if (isPlaying == true && SceneManager.GetActiveScene().name == "Menu")
+        {
+            musicSource.clip = backgroundmusicMenu;
+            musicSource.Play();
+            isPlaying = false;
+        }
+
+
+
+
+        /* if (musicDuration <= goalTime)
+        {
+            musicSource.clip = breachBackgroundLoop;
+            musicSource.Play();
+
+        }
+
+        
+        if (AudioSettings.dspTime > goalTime)
+        {
+            musicSource.clip = breachBackgroundLoop;
+            musicSource.PlayScheduled(goalTime);
+        }
+        */
+
+    }
+
+
 
     public void PlaySFX(AudioClip clip)
     {
