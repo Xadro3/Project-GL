455d293ff61fa2a33b4c13d0a6e83231b9b405ba
sagtdasnicht <mario.viktor.schneider@gmail.com>
2023-12-27 11:01:33 +0100

    ParticleTrigger Script Setup, More Particles

---
+++ Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    public ParticleSystem alpha;
+    public ParticleSystem beta;
+    public ParticleSystem gamma;
+
+    private void Update()//Ganze update ist nur für Testzwecke drin, kann also deleted werden wenn die Eventtrigger implementiert wurden.
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            alpha.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            beta.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            gamma.Play();
+        }
+    }
+
+    void HandleAlphaRadiation()
+    {
+        alpha.Play();
+    }
+
+    void HandleBetaRadiation()
+    {
+        beta.Play();
+    }
+    void HandleGammaRadiation()
+    {
+        gamma.Play();
+    }
+
+}
