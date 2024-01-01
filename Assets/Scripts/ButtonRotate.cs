e0302ccada8cbdb9b555654f6c90999caa2213ad
Ange <Angelo_Fric@hotmail.de>
2024-01-01 23:43:02 +0100

    EndTurnButton adjusted
    Lights now change back to Player after Enemy turn.

--- Assets/Scripts/ButtonRotate.cs
+++ Assets/Scripts/ButtonRotate.cs
@@ -6,14 +6,20 @@ public class ButtonRotate : MonoBehaviour
 {
     [SerializeField]
     private GameObject drehKnopf;
+    public GameObject PlayerTurn;
+    public GameObject EnemyTurn;
 
     public void RotateKnopf()
     {
+        PlayerTurn.SetActive(false);
         drehKnopf.transform.Rotate(0f, 0f, -84);
+        EnemyTurn.SetActive(true);
     }
 
     public void RotateKnopfBack()
     {
+        EnemyTurn.SetActive(false);
         drehKnopf.transform.Rotate(0f, 0f, 84);
+        PlayerTurn.SetActive(true);
     }
 }
