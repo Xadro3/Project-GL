237f2c0c2de53277f195eaa493246d7d2453efcb
Ange <Angelo_Fric@hotmail.de>
2024-01-04 05:19:56 +0100

    Updated Pendants
    Pendants description can now be opened by right clicking a pendant in the encounter scene.
    This is only possible during encounter.

--- Assets/Scripts/PendantScript.cs
+++ Assets/Scripts/PendantScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PendantScript : MonoBehaviour
 {
@@ -11,26 +12,52 @@ public class PendantScript : MonoBehaviour
     public string description;
     public SpriteRenderer spriteRenderer;
     public PendantManager homeBase;
-
+    public GameObject infoPopup;
+    
     [Header("Effect Info")]
     public int pendantEffectValue;
     public GameConstants.pendantEffect pendantEffectType;
     public bool isActive = false;
     public bool isInEffect = false;
 
+
     private void Awake()
     {
         homeBase = FindObjectOfType<PendantManager>();
         OverworldSceneChanger.SceneChangeEvent += EveryoneGetInHere;
         ButtonManager.SceneChangeEvent += EveryoneGetInHere;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+    }
+
+    private void OnSceneUnloaded(Scene scene)
+    {
+        if (scene.name == "Encounter" && isActive)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode arg1)
+    {
+        if (scene.name == "Encounter" && isActive)
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
     }
 
     private void EveryoneGetInHere()
     {
         //Bringing Pendants to the safety of the Wallet :>
         gameObject.transform.SetParent(homeBase.transform);
+        GetComponent<BoxCollider2D>().enabled = false;
     }
 
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
+    }
     private void OnDestroy()
     {
         OverworldSceneChanger.SceneChangeEvent -= EveryoneGetInHere;
@@ -40,6 +67,27 @@ public class PendantScript : MonoBehaviour
     private void Start()
     {
         
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ShowInfoPopup();
+        }
+    }
+
+    private void ShowInfoPopup()
+    {
+        // Instantiate the card popup prefab
+        GameObject cardPopup = Instantiate(infoPopup, transform.position, Quaternion.identity);
+
+        // Set the card popup content (you can customize this based on your card's data)
+        CardPopup popupScript = cardPopup.GetComponent<CardPopup>();
+        if (popupScript != null)
+        {
+            popupScript.SetCardInfo(description);
+        }
     }
 
     public void SetPendantActive(bool b)
