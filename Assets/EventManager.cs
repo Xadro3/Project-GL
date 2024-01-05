37bb53ef57c7b8c08f9957f8347ee1f64296369a
Fabio <fmaier67@gmail.com>
2024-01-05 14:40:42 +0100

    Event 95%
    Do we need rewards?

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -11,37 +11,43 @@ public class EventManager : MonoBehaviour
     public GameObject event_3;
     public GameObject event_4;
     public GameObject event_5;
-
+    public int currentEvent;
     void Start()
     {
         eventSafe = GameObject.FindGameObjectWithTag("EventSafe").GetComponent<EventSafe>();
-        eventSafe.eventCounter = eventSafe.eventCounter++;
-
+        currentEvent =eventSafe.GetRandomEvenWithFalseValue();
+        PopulateScene(currentEvent);
     }
 
     void PopulateScene(int eventCounter)
     {
         switch (eventCounter)
         {
-            case 1:
+            case 0:
                 Instantiate(event_1);
                 break;
-            case 2:
+            case 1:
                 Instantiate(event_2);
                 break;
-            case 3:
+            case 2:
                 Instantiate(event_3);
                 break;
-            case 4:
+            case 3:
                 Instantiate(event_4);
                 break;
-            case 5:
+            case 4:
                 Instantiate(event_5);
                 break;
 
         }
 
     }
+    public void CompleteEvent(int key)
+    {
+        eventSafe.CompleteEvent(key);
+    }
+
+
 
  
 }
--- Assets/EventSolver.cs
+++ Assets/EventSolver.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class EventSolver : MonoBehaviour
 {
-    GameObject backButton;
-    GameObject answer;
-    GameObject wrongAnswer;
-    GameObject question;
-
+   public GameObject backButton;
+   public GameObject answer;
+   public GameObject wrongAnswer;
+   public GameObject question;
+   public EventManager eventManager;
+    private void Start()
+    {
+        eventManager = GameObject.FindGameObjectWithTag("Eventmanager").GetComponent<EventManager>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Eventcard")
         {
+            Debug.Log("Collision");
+            collision.collider.GetComponent<Drag>().startPos = transform.position;
             if (collision.collider.GetComponent<EventCard>().isCorrectAnswer)
             {
                 backButton.SetActive(true);
                 answer.SetActive(true);
                 question.SetActive(false);
+                eventManager.CompleteEvent(eventManager.currentEvent);
 
             }
             else
@@ -25,6 +32,7 @@ public class EventSolver : MonoBehaviour
                 wrongAnswer.SetActive(true);
                 question.SetActive(false);
                 backButton.SetActive(true);
+                eventManager.CompleteEvent(eventManager.currentEvent);
             }
         }
     }
