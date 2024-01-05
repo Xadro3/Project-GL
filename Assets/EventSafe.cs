092012ff556c48def9b89dd50649390c397682ae
Fabio <fmaier67@gmail.com>
2024-01-05 14:40:42 +0100

    Event 95%
    Do we need rewards? Need to Add EventSafe to Wallet, it is in the scene for testing ONLY

--- Assets/EventSafe.cs
+++ Assets/EventSafe.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EventSafe : MonoBehaviour
@@ -16,4 +17,30 @@ public class EventSafe : MonoBehaviour
         events.Add(5, false);
     }
 
+    public int GetRandomEvenWithFalseValue()
+    {
+        var eventFalseEvents = events.Where(entry => entry.Key % 2 == 0 && entry.Value == false).ToList();
+
+        if (eventFalseEvents.Count == 0)
+        {
+            
+            return -1; // You can return a specific value or handle this case accordingly
+        }
+
+        int randomIndex = UnityEngine.Random.Range(0, eventFalseEvents.Count);
+        return eventFalseEvents[randomIndex].Key;
+    }
+
+    public void CompleteEvent(int key)
+    {
+        if (events.ContainsKey(key))
+        {
+            events[key] = true;
+            Debug.Log("Event " + key + " completed!");
+        }
+        else
+        {
+            Debug.Log("Event with key " + key + " not found in the dictionary.");
+        }
+    }
 }
--- Assets/QuestionMaster.cs
+++ Assets/QuestionMaster.cs
@@ -8,12 +8,17 @@ public class QuestionMaster : MonoBehaviour
     public string questionText;
     public string answerText;
     public string wrongAnswerText;
+    public EventSolver solver;
     public GameObject answer;
     public GameObject wrongAnswer;
+
     // Start is called before the first frame update
     void Start()
     {
         GameObject.FindGameObjectWithTag("Question").GetComponent<TextMeshProUGUI>().text = questionText;
+        solver = GameObject.FindGameObjectWithTag("Eventslot").GetComponent<EventSolver>();
+        answer = solver.answer;
+        wrongAnswer = solver.wrongAnswer;
         answer.GetComponent<TextMeshProUGUI>().text = answerText;
         wrongAnswer.GetComponent<TextMeshProUGUI>().text = wrongAnswerText;
     }
