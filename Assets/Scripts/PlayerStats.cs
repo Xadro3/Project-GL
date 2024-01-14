501ae9298036adf91fd217928f7fb2891e86d012
Ange <Angelo_Fric@hotmail.de>
2024-01-14 14:03:46 +0100

    Updated Overworld Display
    Alpha and Beta now also is correctly displayed on overworld

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,8 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
-    private int health;
-    private int gammaResistance;
+    public int health;
+    public int healthMax;
+    public int gammaResistance;
+    public int gammaResistanceMax;
+    public int betaResistance;
+    public int betaResistanceMax;
+    public int alphaResistance;
+    public int alphaResistanceMax;
+    public List<HealthBar> healthBars;
     PlayerHealthManager playerInfo = null;
 
     private void OnEnable()
@@ -40,15 +47,21 @@ public class PlayerStats : MonoBehaviour
         if (playerInfo != null)
         {
             Debug.Log("Found PlayerHealthManager");
+            healthMax = playerInfo.healthMax;
             health = playerInfo.health;
+            gammaResistanceMax = playerInfo.gammaResistanceMax;
             gammaResistance = playerInfo.gammaResistance;
+            betaResistanceMax = playerInfo.betaResistanceMax;
+            betaResistance = playerInfo.betaResistance;
+            alphaResistanceMax = playerInfo.alphaResistanceMax;
+            alphaResistance = playerInfo.alphaResistance;
         }
     }
 
     private void UpdateUIElements()
     {
         Debug.Log("Updating UI Elements");
-        List<HealthBar> healthBars = new List<HealthBar>();  
+        healthBars.Clear();
         healthBars.AddRange(FindObjectsOfType<HealthBar>());
 
         if (healthBars.Count > 0)
@@ -58,10 +71,20 @@ public class PlayerStats : MonoBehaviour
                 switch (bar.gameObject.name)
                 {
                     case "GammaBar":
+                        bar.SetMaxHealth(gammaResistanceMax);
                         bar.SetHealth(gammaResistance);
                         break;
                     case "HealthBar":
+                        bar.SetMaxHealth(healthMax);
                         bar.SetHealth(health);
+                        break;
+                    case "AlphaBar":
+                        bar.SetMaxHealth(alphaResistanceMax);
+                        bar.SetHealth(alphaResistance);
+                        break;
+                    case "BetaBar":
+                        bar.SetMaxHealth(betaResistanceMax);
+                        bar.SetHealth(betaResistance);
                         break;
                     default:
                         Debug.Log("No Bars");
